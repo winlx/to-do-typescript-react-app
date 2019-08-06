99f28ec8b90bd7aef26a4c6e2617e879a5e79cd5
Aleksei Iakovets <aleksei@winlx.com>
2019-08-06 11:08:54 +0300

    Updates

---
+++ precache-manifest.23cfba277eb3f8f4a50816a546a8cdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebd34b4afdf60a2e276fc3582818426",
+    "url": "/to-do-typescript-react-app/index.html"
+  },
+  {
+    "revision": "9e650d0aa17f5773f321",
+    "url": "/to-do-typescript-react-app/static/css/2.2601abe2.chunk.css"
+  },
+  {
+    "revision": "90b91c61a5fe6b6bfdcb",
+    "url": "/to-do-typescript-react-app/static/css/main.1feb1011.chunk.css"
+  },
+  {
+    "revision": "9e650d0aa17f5773f321",
+    "url": "/to-do-typescript-react-app/static/js/2.cf4f7837.chunk.js"
+  },
+  {
+    "revision": "90b91c61a5fe6b6bfdcb",
+    "url": "/to-do-typescript-react-app/static/js/main.eb466c6a.chunk.js"
+  },
+  {
+    "revision": "b1dfffc677a0028c6892",
+    "url": "/to-do-typescript-react-app/static/js/runtime~main.8d553b13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17d707b477549fe3304b2da76c0e3bcd.js"
+  "/to-do-typescript-react-app/precache-manifest.23cfba277eb3f8f4a50816a546a8cdf4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/to-do-typescript-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
