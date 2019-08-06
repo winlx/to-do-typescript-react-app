13e5aaff27c001c88fa1744bcea9f88047fc5055
Aleksei Iakovets <aleksei@winlx.com>
2019-08-06 10:55:53 +0300

    Updates

---
+++ precache-manifest.17d707b477549fe3304b2da76c0e3bcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be92212e444fc0450caefdd3257e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f01b8ff108f02c06b5",
+    "url": "/static/css/2.2601abe2.chunk.css"
+  },
+  {
+    "revision": "90b91c61a5fe6b6bfdcb",
+    "url": "/static/css/main.1feb1011.chunk.css"
+  },
+  {
+    "revision": "56f01b8ff108f02c06b5",
+    "url": "/static/js/2.5ba557e6.chunk.js"
+  },
+  {
+    "revision": "90b91c61a5fe6b6bfdcb",
+    "url": "/static/js/main.eb466c6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
